e4e36456a844fbe9041d68b656c99ee0908a3dba
Arthur Lebron <78775633+SongXiaoYi@users.noreply.github.com>
2026-02-09 10:24:37 +0800

    Update S05.Time_alignment.R

--- S05.Time_alignment.R
+++ S05.Time_alignment.R
@@ -30,25 +30,54 @@ suppressMessages(library(SingleCellExperiment))
 suppressMessages(library(destiny))
 suppressMessages(library(plotly))
 ######################
-setwd('G:\\YouRui')
-pbmc_CD8T <- readRDS('G:\\YouRui\\CD8Tcell\\CD8T.rds')
+setwd('I:\\肿瘤脑转移\\Experiment\\data')
+pbmc <- readRDS('Cytotrace_pbmc.rds')
 
-##################################################### Subcluster
-Idents(pbmc_CD8T) <- pbmc_CD8T$cell                                       
-pbmc_PC <- subset(pbmc_CD8T, idents = c('Tex_IFIT1','CTL_CLNK','Tem_SLC4A10'))                                       
-rm(pbmc_CD8T)
-gc()
+pbmc <- NormalizeData(object = pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
+pbmc <- FindVariableFeatures(object = pbmc, selection.method = "vst", nfeatures = 2500)
+all.genes <- rownames(pbmc)
+pbmc <- ScaleData(pbmc, features = all.genes)
+pbmc <- RunPCA(object= pbmc,npcs = 20,pc.genes=VariableFeatures(object = pbmc))   #这里PCA设置为前100个
+
+#开始去批次
+pbmc$orig.ident <- pbmc$sample
+pbmc <- RunHarmony(pbmc,"orig.ident",dims.use = 1:20,max.iter.harmony = 15,max.iter.cluster = 20)   #去批次这一部看结果
+pbmc <- RunUMAP(pbmc, dims = 1:20, min.dist = 0.2,reduction = "harmony")   #尝试调整min.dist会得到更好的结果
+pbmc <- FindNeighbors(pbmc, reduction = "harmony",dims = 1:20)
+
+pbmc <- FindClusters(pbmc, resolution = 0.4)   
+
+
+annotation <- data.frame(phenotype=pbmc@meta.data$seurat_clusters) %>% set_rownames(., colnames(pbmc))
+plots <- plotData(cytotrace2_result = pbmc, 
+                  annotation = annotation,
+                  expression_data = pbmc,is_seurat = TRUE
+                  )
+
+#pbmc <- RunTSNE(pbmc, dims = 1:20, tsne.method = "FIt-SNE", nthreads = 4, max_iter = 2000)    #尝试调整max_iter会得到更好的结果
+pbmc <- RunUMAP(pbmc, dims = 1:20, min.dist = 0.75)
+Idents(pbmc) <- pbmc$seurat_clusters
+##################################################### Subcluster                                   
+pbmc_subset <- subset(pbmc, idents = c(5,3,4,2,0))                                       
+
+Idents(pbmc_subset) <- pbmc_subset$seurat_clusters
+pbmc <- pbmc_subset
+
+new.cluster.ids <- c("Differentiated", "Unipotent", "Multipotent", "Oligopotent", "Pluripotent")
+names(new.cluster.ids) <- levels(pbmc)
+pbmc <- RenameIdents(pbmc, new.cluster.ids)
+pbmc$Diff <- Idents(pbmc)
 ########################
-coembed <- pbmc_PC
+coembed <- pbmc
 ##############################
 df <- coembed@meta.data %>%
     as.data.frame() %>%
-    subset(., cell %in% c("Tem_SLC4A10", "CTL_CLNK", "Tex_IFIT1"))
+    subset(., Diff %in% c("Differentiated", "Unipotent", "Multipotent", "Oligopotent", "Pluripotent"))
 
 coembed <- coembed[, rownames(df)]
-cols <- ArchR::paletteDiscrete(unique(coembed@meta.data[, "cell"]))
+cols <- ArchR::paletteDiscrete(unique(coembed@meta.data[, "Diff"]))
 ###################################
-traj1 <- c("Tem_SLC4A10", "CTL_CLNK", "Tex_IFIT1")
+traj1 <- c("Differentiated", "Unipotent", "Oligopotent", "Multipotent", "Pluripotent")
 #######################
 coembed <- RunUMAP(coembed, 
                dims = 1:20, 
@@ -59,8 +88,8 @@ coembed <- RunUMAP(coembed,
                     min.dist = 0.4)
 ####################################################
 coembed <- AddTrajectory(object = coembed, 
-                          trajectory = c("Tem_SLC4A10", "CTL_CLNK", "Tex_IFIT1"),
-                          group.by = "cell", 
+                          trajectory = c("Differentiated", "Unipotent", "Oligopotent", "Multipotent", "Pluripotent"),
+                          group.by = "Diff", 
                           reduction = "harmony_v2",
                           dims = 1:2, 
                           use.all = TRUE)                                   
@@ -68,7 +97,7 @@ coembed <- AddTrajectory(object = coembed,
 coembed <- coembed[, !is.na(coembed$Trajectory)]
 
 p1 <- DimPlot(coembed, reduction = "harmony_v2", 
-              group.by = "cell", cols = cols) +
+              group.by = "Diff", cols = cols) +
               xlab("UMAP 1") + ylab("UMAP 2") +
     ggtitle("Cluster")
 
